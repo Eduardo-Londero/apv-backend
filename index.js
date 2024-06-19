bb2cdb2e381e7d3d895bd25e08118e041ca0af78
Eduardo <edulondero2407@gmail.com>
2024-06-18 22:36:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from 'dotenv'
+import cors from 'cors'
+import conectarDB from "./config/db.js";
+import VeterinarioRoutes from './routes/veterinarioRoutes.js'
+import PacienteRoutes from './routes/pacienteRoutes.js'
+
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+conectarDB();
+
+const dominiosPermitidos = [process.env.FRONTEND_URL]
+
+const corsOptions = {
+    origin: function (origin, callback){
+        if(dominiosPermitidos.indexOf(origin) !== -1 ){
+            //El origen del Request esta permitido
+            callback(null, true)
+        } else{
+            callback(new Error('No permitido por CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
+
+
+
+app.use('/api/veterinarios', VeterinarioRoutes );
+app.use('/api/pacientes', PacienteRoutes );
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(4000, ()=>{
+    console.log(`Servidor funcionando en el puerto ${PORT}`);
+});
